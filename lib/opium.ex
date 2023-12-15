2a891a86ad912c62c40ceaab250209f07d72b8ba
Cyytrus <antoniopaulo122@gmail.com>
2023-12-15 15:12:54 -0300

    first commit :rocket:

---
+++ lib/opium.ex
@@ -0,0 +1,18 @@
+defmodule Opium do
+  @moduledoc """
+  Documentation for `Opium`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Opium.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/opium_test.exs
@@ -0,0 +1,8 @@
+defmodule OpiumTest do
+  use ExUnit.Case
+  doctest Opium
+
+  test "greets the world" do
+    assert Opium.hello() == :world
+  end
+end
